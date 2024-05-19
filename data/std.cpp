3f6e4e9e92a40609ec8c6a871b7ed851755b5396
xx-luozi-xx <xx_luozi_xx@qq.com>
2024-05-19 13:45:03 +0800

    std

--- data/std.cpp
+++ data/std.cpp
@@ -1,105 +1,98 @@
+#include <list>
 #include <iostream>
-#include <vector>
-#include <math.h>
-#include <assert.h>
+#include <algorithm>
 using namespace std;
-const int MAX_SIZE = 100+30;
-int n, P;
-vector<int> eage[MAX_SIZE];
-int fa[MAX_SIZE];
+const int MAX_SIZE = 1e5 +30;
 
-struct Vec3{
-    double x, y, z;
-    Vec3()=default;
-    Vec3(double x, double y, double z):x(x),y(y),z(z){}
-    Vec3(const Vec3& p1, const Vec3& p2):
-        x(p2.x-p1.x),y(p2.y-p1.y),z(p2.z-p1.z){}
-    double len(){return sqrt(x*x+y*y+z*z);}
-    double len2(){return x*x+y*y+z*z;}
-    Vec3 X(const Vec3& rhs){
-        return {
-            y*rhs.z-z*rhs.y,
-            z*rhs.x-x*rhs.z,
-            x*rhs.y-y*rhs.x
-        };
-    }
-    double cdot(const Vec3& rhs){
-        return x*rhs.x+y*rhs.y+z*rhs.z;
-    }
-    Vec3 times(double num){
-        return {num*x, num*y, num*z};
-    }
-}F;
+struct Ring{
+    list<int> nodes;
+    int val = 0;
+    int max = 0;
+    int min = 0;
 
-Vec3 pos[MAX_SIZE];
-double S[MAX_SIZE];
-void make_fa(int root);
-void make_S(int point, Vec3 f);
-void init();
+    void formal();
+};
+
+int n;
+list<int> edge[MAX_SIZE];
+int nxt[MAX_SIZE];
+
+list<Ring> rings;
+
+int color[MAX_SIZE];
+
+int dfs(int src);
+
 int main(){
-    int T; cin >> T;
-    again_:init();
-
-    T--;
     cin >> n;
+    for(int i = 0; i < n; ++i){
+        int a, b;
+        cin >> a >> b;
+        nxt[a] = b;
+        edge[a].push_back(b);
+        edge[b].push_back(a);
+    }
+
     for(int i = 1; i <= n; ++i){
-        cin >> pos[i].x >> pos[i].y >> pos[i].z;
+        int tag = i;
+        int now = i;
+        while(color[now] == 0){
+            color[now] = tag;
+            now = nxt[now];
+        }
+        if(color[now] == tag){//ring
+            list<int> new_ring;
+            new_ring.push_back(now);
+            int itr = nxt[now];
+            while(itr != now){
+                new_ring.push_back(itr);
+                itr = nxt[itr];
+            }
+            rings.push_back({new_ring,0,0,0});
+            rings.back().formal();
+        }
     }
-    //建图 
-    for(int i = 1; i < n; ++i){
-        int a, b; cin >> a >> b;
-        eage[a].push_back(b);
-        eage[b].push_back(a);
+
+    Ring* ans = &(rings.front());
+    for(auto itr = rings.begin(); itr != rings.end(); ++itr){
+        if(itr->val > ans->val){
+            ans = &(*itr);
+        }else if(itr->val == ans->val && itr->max > ans->max){
+            ans = &(*itr);
+        }
     }
-    
-    cin >> P;
-    cin >> F.x >> F.y >> F.z;
-    //以P为根建树
-    make_fa(P);
-    //计算所有
-    make_S(P, F);
-    //输出
-    for(int i = 1; i <= n; ++i){
-        printf("%lf\n", S[i]);
-        // cout << S[i] << '\n';
+
+    for(int num : ans->nodes){
+        cout << num << ' ';
     }
-    if(T) goto again_;
     return 0;
 }
 
-void make_fa(int root){
-    for(int i = 0; i < (int)(eage[root].size()); ++i){
-        if(eage[root][i] != fa[root]){
-            fa[eage[root][i]] = root;
-            make_fa(eage[root][i]);
+void Ring::formal(){
+    min = max = nodes.front();
+
+    for(int num : nodes){
+        max = ::max(max, num);
+        min = ::min(min, num);
+    }
+
+    while(nodes.front() != min){
+        nodes.push_back(nodes.front());
+        nodes.pop_front();
+    }
+
+    val = 2*nodes.size() - dfs(nodes.front());
+}   
+
+
+bool vit[MAX_SIZE];
+int dfs(int src){
+    vit[src] = true;
+    int ret = 1;
+    for(auto b : edge[src]){
+        if(vit[b] == false){
+            ret += dfs(b);
         }
     }
-}
-
-void make_S(int point, Vec3 f){
-    bool tran = 0;
-    for(int i = 0; i < (int)(eage[point].size()); ++i){
-        if(eage[point][i] != fa[point]){
-            tran = 1;
-            Vec3 Pq(pos[point], pos[eage[point][i]]);
-            assert(Pq.len2()!=0);
-            Vec3 next_f = Pq.times(f.cdot(Pq)/Pq.len2());
-            S[point] += f.X(next_f).len();
-            make_S(eage[point][i], next_f);
-        }
-    }
-    if(tran == 0){
-        S[point] = f.len();
-    }
-}
-
-void init(){
-    for(int i = 0; i < MAX_SIZE; ++i){
-        eage[i] = vector<int>();
-        fa[i] = int();
-        pos[i] = Vec3();
-        S[i] = double();
-    }
-    F = Vec3();
-    n = P = 0;
+    return ret;
 }
